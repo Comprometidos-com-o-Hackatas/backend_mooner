62638c883d4313bec3646c5d7b3396452ede6664
luansilva25 <luansilva250807@gmail.com>
2024-11-01 18:39:47 -0300

    build updated

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 set -o errexit
+export DJANGO_SETTINGS_MODULE="src.config.settings"
+export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/backend_mooner/src
 pip install --upgrade pip
 pip install -r requirements.txt
 python src/manage.py collectstatic --no-input
 python src/manage.py migrate
-export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/backend_mooner/src
