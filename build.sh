6fc53fdbb0c8f04ea691cd791ddcc05d73aba655
luansilva25 <luansilva250807@gmail.com>
2024-11-01 18:28:44 -0300

    build updated

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python src/manage.py collectstatic --no-input
 python src/manage.py migrate
+export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/backend_mooner/src
