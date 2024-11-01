f9e5c0c613a3c05027df6ab1234bc92d1be603b8
luansilva25 <luansilva250807@gmail.com>
2024-11-01 11:16:41 -0300

    build upadted

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+python src/manage.py collectstatic --no-input
+python src/manage.py migrate
