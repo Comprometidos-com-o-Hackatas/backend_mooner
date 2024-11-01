0987dfb318f3d5b31101a1e8ccbd9938d24ffeb9
luansilva25 <luansilva250807@gmail.com>
2024-11-01 20:41:02 -0300

    requirements.txt uploaded

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -o errexit
-export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/src
 pip install --upgrade pip
 pip install -r requirements.txt
 python src/manage.py collectstatic --no-input
 python src/manage.py migrate
+export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/src
