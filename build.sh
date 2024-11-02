cd92a0f4a00a7359320b0f7b790c3a34353adcae
luansilva25 <luansilva250807@gmail.com>
2024-11-01 21:02:52 -0300

    asgi uploaded

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python src/manage.py collectstatic --no-input
 python src/manage.py migrate
-export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/src
