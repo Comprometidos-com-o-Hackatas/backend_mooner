087a998bb271a01cea7bfd8baf0462d20f8a1d71
Gabz047 <gabe1612mail@gmail.com>
2024-11-08 07:48:55 -0300

    Feat: Added things

--- src/core/mooner/serializers/playlist.py
+++ src/core/mooner/serializers/playlist.py
@@ -1,9 +1,13 @@
 from ..models import Playlist
 from core.usuario.models import Usuario
+from core.uploader.models import Image
+from core.mooner.models import Song
 from rest_framework import serializers
 
 class PlaylistCreateSerializer(serializers.ModelSerializer):
-    owners = serializers.PrimaryKeyRelatedField(queryset=Usuario.objects.all(), many=True)  # Definindo o campo
+    owners = serializers.PrimaryKeyRelatedField(queryset=Usuario.objects.all(), many=True)
+    songs = serializers.PrimaryKeyRelatedField(queryset=Song.objects.all(), many=True) 
+    # cover = serializers.SlugRelatedField(slug_field=;) # Definindo o campo
     class Meta:
         model = Playlist
         fields = ['name', 'songs', 'owners']
@@ -11,7 +15,7 @@ class PlaylistCreateSerializer(serializers.ModelSerializer):
 class PlaylistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Playlist
-        fields = ['name', 'songs', 'owners']
+        fields = ['id', 'name', 'songs', 'owners', 'cover']
         depth = 2
 
     
