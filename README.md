24dfde5a1b5a06836a59c2b0f82bf35aabb45171
jaotarzan <joaovssouza59@gmail.com>
2024-12-02 15:35:15 -0300

    FIX: Fixing the ws urls

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # backend_hackaton
+1. Crie um ambiente virtual usando o [PDM](https://pdm.fming.dev/):
+
+   ```
+   pdm install
+   ```
+
+2. Execute o servidor de desenvolvimento:
+
+   ```
+   pdm run dev
+   ```
+
+3. Execute o websocket:
+
+    ```
+    cd src/
+    pdm run daphne -p 8001 config.asgi:application
+    ```
--- src/core/moon_wave/consumers.py
+++ src/core/moon_wave/consumers.py
@@ -1,14 +1,17 @@
 import json
+import re
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class MoonWaveConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_group_name = self.scope["url_route"]["kwargs"]["room_name"]
+        original_room_name = self.scope["url_route"]["kwargs"]["room_name"]
 
+        self.room_group_name = re.sub(r"[^a-zA-Z0-9_.-]", "_", original_room_name)
+
+        print(original_room_name, self.room_group_name)
          # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
 
     async def disconnect(self, close_code):
@@ -19,11 +22,13 @@ class MoonWaveConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         try:
             data_json = json.loads(text_data)
+            song = data_json.get('song', None)
             action = data_json.get('action', None)
             timestamp = data_json.get('timestamp', None)
+            queue = data_json.get('queue', None)
 
             await self.channel_layer.group_send(
-                self.room_group_name, {"type": "moon_wave_message", "action": action, "timestamp": timestamp}
+                self.room_group_name, {"type": "moon_wave_message", "action": action, "timestamp": timestamp, "song": song, "queue": queue}
             )
 
         except json.JSONDecodeError as e:
@@ -32,5 +37,8 @@ class MoonWaveConsumer(AsyncWebsocketConsumer):
     async def moon_wave_message(self, event):
         action = event["action"]
         timestamp = event["timestamp"]
+        song = event["song"]
+        queue = event["queue"]
 
-        await self.send(text_data=json.dumps({'action': action, 'timestamp': timestamp}))
+
+        await self.send(text_data=json.dumps({'action': action, 'timestamp': timestamp, 'song': song, 'queue': queue}))
